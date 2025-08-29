a4328e114627d44af0937b994c7f66d052be3b68
Nitish Kumar <nitishsinghjtpr@gmail.com>
2025-08-29 13:58:58 +0530

    add code

---
+++ Inheritance/Animal.java
@@ -0,0 +1,37 @@
+//ek program me ek public ho to
+
+package Inheritance;
+
+// class Animal {
+//     void eat(){
+//         System.out.println("I am eation");
+//     }
+// }
+// public class Dog extends Animal{
+//     void bark(){
+//         System.out.println("barking");
+//      }
+//      public static void main(String[] args) {
+//         Dog obj=new Dog();
+//         obj.bark();
+//         obj.eat();
+//     }
+// }
+
+
+public class Animal {
+   void eat(){
+        System.out.println("I am eation");
+    }
+
+static class Dog extends Animal{
+   void bark(){
+        System.out.println("barking");
+     }
+    }
+     public static void main(String[] args) {
+        Dog obj=new Dog();
+        obj.bark();
+        obj.eat();
+    }
+}
